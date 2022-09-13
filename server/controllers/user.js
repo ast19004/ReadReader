fb01cdca0ae75ef82890061f6c4b21fd3ef36ced
ast19004 <ast19004@byui.edu>
2022-09-12 20:31:22 -0700

    Added jwt & isAuth Middleware

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
 
 
 const { validationResult } = require('express-validator');
@@ -36,4 +37,39 @@ exports.postSignup = (req, res, next) => {
 exports.postLogin = (req, res, next) => {
     const email = req.email;
     const password = req.password;
+
+    User.findOne({email: email})
+    .then((user) => {
+        if(user){
+            bcrypt.compare(password, user.password)
+            .then((result) => {
+                if(result){
+                    const token = jwt.sign(
+                        {
+                            userId: user._id.toString(),
+                            email: user.email
+                        },
+                        process.env.JWT_SECRET,
+                        {expiresIn: "1h"}
+                    );
+                    res.status(200).json({
+                        message: "Successfully Logged In",
+                        userId: user._id.toString(),
+                        token: token,
+                    });
+                } else{
+                    res.status(400).json({ message: "Invalid User Information"})
+                }
+            });
+        } else{
+            res.status(404).json({
+                message: "User not found"
+            });
+        }
+    })
+    .catch( (err) => {
+        const error = new Error(err);
+        error.httpStatusCode = 500;
+        return next(err);
+    });
 }
