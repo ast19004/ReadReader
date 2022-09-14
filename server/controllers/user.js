07af393274573e42014842c1b6696168604d144a
ast19004 <ast19004@byui.edu>
2022-09-13 20:37:44 -0700

    User login functional in Swagger test

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -43,8 +43,8 @@ exports.postSignup = (req, res, next) => {
 }
 
 exports.postLogin = (req, res, next) => {
-    const email = req.email;
-    const password = req.password;
+    const email = req.body.email;
+    const password = req.body.password;
 
     const errors = validationResult(req);
     if(!errors.isEmpty()){
