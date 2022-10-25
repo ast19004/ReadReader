6df18639d1e263b9ad10e42dc4761c692aa5b7fe
Anna Astle <ast19004@byui.edu>
2022-10-24 18:29:45 -0700

    AddPrizeIcon moved

--- client/read-reader/src/components/Layout/AccountMenu.js
+++ client/read-reader/src/components/Layout/AccountMenu.js
@@ -1,209 +1,234 @@
-import React, { useState, useContext, useEffect } from 'react';
-import { useHistory } from "react-router-dom"
+import React, { useState, useContext, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
-import AuthContext from '../../store/auth-contex';
-import ReaderContext from '../../store/reader-contex';
+import AuthContext from "../../store/auth-contex";
+import ReaderContext from "../../store/reader-contex";
 
-import { Menu, MenuItem, IconButton, Avatar, Divider, ListItemIcon } from '@mui/material';
-import {Person, PersonAdd} from '@mui/icons-material';
-import LogoutIcon from '@mui/icons-material/Logout';
-import PrizesIcon from '@mui/icons-material/EmojiEvents';
-import AddPrizeIcon from '../UI/AddPrizeIcon';
+import {
+  Menu,
+  MenuItem,
+  IconButton,
+  Avatar,
+  Divider,
+  ListItemIcon,
+} from "@mui/material";
+import { Person, PersonAdd } from "@mui/icons-material";
+import LogoutIcon from "@mui/icons-material/Logout";
+import PrizesIcon from "@mui/icons-material/EmojiEvents";
+import AddPrizeIcon from "../Prize/AddPrizeIcon";
 
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const AccountMenu = () => {
-    const authCtx = useContext(AuthContext);
-    const readerCtx = useContext(ReaderContext);
+  const authCtx = useContext(AuthContext);
+  const readerCtx = useContext(ReaderContext);
 
-    const history = useHistory();
+  const history = useHistory();
 
-    const [anchorEl, setAnchorEl] = useState(null);
-    const [readers, setReaders] = useState();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [readers, setReaders] = useState();
 
-    // if "" user == mainUser
-    const[currentReader, setCurrentReader] = useState("");
+  // if "" user == mainUser
+  const [currentReader, setCurrentReader] = useState("");
 
+  const userMenuOpen = !!anchorEl;
 
-    const userMenuOpen = !!anchorEl;
+  const handleMenuClick = (event) => {
+    setAnchorEl(event.target);
+  };
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+  const returnToMainUser = () => {
+    readerCtx.onChangeReaderName("");
+    readerCtx.onChangeReaderId("");
+  };
 
-    const handleMenuClick = (event) => {
-        setAnchorEl(event.target);
+  const handleAddReader = () => {
+    returnToMainUser();
+    history.push("/reader");
+  };
+
+  const handleAddPrize = () => {
+    returnToMainUser();
+    history.push("/prize");
+  };
+
+  const handleViewPrizes = () => {
+    returnToMainUser();
+    history.push("/prizes/");
+  };
+
+  const handleSelectMainUser = () => {
+    returnToMainUser();
+    history.push("/");
+  };
+
+  useEffect(() => {
+    const url = "http://localhost:5000/readers";
+    const requestOptions = {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: "Bearer " + authCtx.token,
+      },
     };
-    const handleMenuClose = () => {
-        setAnchorEl(null);
-    };
-    const returnToMainUser = () => {
-        readerCtx.onChangeReaderName('');
-        readerCtx.onChangeReaderId('');
-    };
 
-    const handleAddReader = () => {
-        returnToMainUser();
-        history.push('/reader');
-    };
+    const fetchReaderData = async () => {
+      const res = await fetch(url, requestOptions);
 
-    const handleAddPrize = () => {
-        returnToMainUser();
-        history.push('/prize');
-    };
+      if (!res.ok) {
+        throw new Error("Something went wrong!");
+      }
+      const resData = await res.json();
 
-    const handleViewPrizes = () => {
-        returnToMainUser();
-        history.push('/prizes/');
-    };
-
-    const handleSelectMainUser = () => {
-        returnToMainUser();
-        history.push('/');
-    }
-
-    useEffect(()=> {
-        const url = "http://localhost:5000/readers";
-        const requestOptions = {
-            method: 'GET',
-            headers: {
-                'Accept': 'application/json',
-                Authorization: 'Bearer ' + authCtx.token
-            }
+      const loadedReaders = resData.readers.map((reader) => {
+        const initials = [...reader["reader_name"]].splice(0, 2).join("");
+        return {
+          id: reader["_id"],
+          name: reader["reader_name"],
+          initials: initials.charAt(0).toUpperCase() + initials.slice(1),
         };
-        
-        const fetchReaderData = async() => {
-            const res = await fetch(url, requestOptions);
+      });
+      setReaders(loadedReaders);
+    };
 
-            if(!res.ok){
-                throw new Error('Something went wrong!');
-            };
-            const resData = await res.json();
-            
-            const loadedReaders = resData.readers.map(reader => {
-                const initials = [...reader['reader_name']].splice(0, 2).join("");
-                return {
-                    id: reader['_id'],
-                    name: reader['reader_name'],
-                    initials: initials.charAt(0).toUpperCase() + initials.slice(1),
-            }
-        });
-            setReaders(loadedReaders);
-            };
+    fetchReaderData().catch((err) => {
+      console.log(err);
+    });
+  }, []);
 
-            fetchReaderData().catch((err) => {
-                console.log(err);
-            });
-    }, []);
+  useEffect(() => {
+    if (readerCtx.currentReaderName !== "") {
+      const initials = [...readerCtx.currentReaderName].splice(0, 2).join("");
+      const capitalizedInitials =
+        initials.charAt(0).toUpperCase() + initials.slice(1);
+      setCurrentReader(capitalizedInitials);
+    } else {
+      setCurrentReader("");
+    }
+  }, [readerCtx.currentReaderId, readerCtx.currentReaderName]);
 
-    useEffect(() => {
-        if(readerCtx.currentReaderName !== ''){
-        const initials = [...readerCtx.currentReaderName].splice(0, 2).join("");
-        const capitalizedInitials = initials.charAt(0).toUpperCase() + initials.slice(1);
-        setCurrentReader(capitalizedInitials);
-        }else{
-            setCurrentReader('');
-        }
-    }, [readerCtx.currentReaderId, readerCtx.currentReaderName]);
-
-    return (
-        <div>
-        <IconButton
-            style={{
-                backgroundColor: "white",
-                borderRadius: "50%",
-                padding: "4px",
-                margin: "0.5rem"
+  return (
+    <div>
+      <IconButton
+        style={{
+          backgroundColor: "white",
+          borderRadius: "50%",
+          padding: "4px",
+          margin: "0.5rem",
+        }}
+        onClick={handleMenuClick}
+        size="medium"
+        aria-controls={userMenuOpen ? "account-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={userMenuOpen ? "true" : undefined}
+      >
+        {!currentReader && <Person fontSize="medium" />}
+        {currentReader && (
+          <Avatar
+            sx={{
+              width: 23,
+              height: 23,
+              color: "black",
+              bgcolor: "white",
+              padding: "1px",
             }}
-            onClick={handleMenuClick}
-            size="medium"
-            aria-controls={userMenuOpen ? 'account-menu' : undefined}
-            aria-haspopup="true"
-            aria-expanded={userMenuOpen ? 'true' : undefined}
-            >
-            { !currentReader && 
-                <Person fontSize='medium'/>
-            }
-            { currentReader && 
-                <Avatar sx={{ width: 23, height: 23, color: 'black', bgcolor: "white", padding: '1px'}}>{currentReader}</Avatar>
-            }
-        </IconButton>
-        <MenuWrapper>
-            <Menu
-                anchorEl={anchorEl}
-                id="user-menu"
-                open={ userMenuOpen }
-                onClose={handleMenuClose}
-                onClick={handleMenuClose}
-                PaperProps={{
-                    elevation: 0,
-                    sx: {
-                        overflow: 'visible',
-                        filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-                        mt: 1.5,
-                        '& .MuiAvatar-root': {
-                        width: 32,
-                        height: 32,
-                        ml: -0.5,
-                        mr: 1,
-                        },
-                        '&:before': {
-                        content: '""',
-                        display: 'block',
-                        position: 'absolute',
-                        top: 0,
-                        right: 14,
-                        width: 10,
-                        height: 10,
-                        bgcolor: 'background.paper',
-                        transform: 'translateY(-50%) rotate(45deg)',
-                        zIndex: 0,
-                        },
-                    },
-            }}
-                transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-                anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-            >
-                <MenuItem onClick={handleSelectMainUser}><Avatar/>USER</MenuItem>
-                <Divider />
-                { readers && readers.map((reader)=>  
-                     <MenuItem key={reader.id} onClick={ () => {readerCtx.onChangeReaderName(reader.name); readerCtx.onChangeReaderId(reader.id); history.push(`/reader/${reader.id}/logReading/`);}}>
-                        <ListItemIcon>
-                            <Avatar>{reader.initials}</Avatar>
-                        </ListItemIcon>
-                            {reader.name}
-                    </MenuItem>
-                )}
-                <MenuItem onClick={handleAddReader}>
+          >
+            {currentReader}
+          </Avatar>
+        )}
+      </IconButton>
+      <MenuWrapper>
+        <Menu
+          anchorEl={anchorEl}
+          id="user-menu"
+          open={userMenuOpen}
+          onClose={handleMenuClose}
+          onClick={handleMenuClose}
+          PaperProps={{
+            elevation: 0,
+            sx: {
+              overflow: "visible",
+              filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+              mt: 1.5,
+              "& .MuiAvatar-root": {
+                width: 32,
+                height: 32,
+                ml: -0.5,
+                mr: 1,
+              },
+              "&:before": {
+                content: '""',
+                display: "block",
+                position: "absolute",
+                top: 0,
+                right: 14,
+                width: 10,
+                height: 10,
+                bgcolor: "background.paper",
+                transform: "translateY(-50%) rotate(45deg)",
+                zIndex: 0,
+              },
+            },
+          }}
+          transformOrigin={{ horizontal: "right", vertical: "top" }}
+          anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+        >
+          <MenuItem onClick={handleSelectMainUser}>
+            <Avatar />
+            USER
+          </MenuItem>
+          <Divider />
+          {readers &&
+            readers.map((reader) => (
+              <MenuItem
+                key={reader.id}
+                onClick={() => {
+                  readerCtx.onChangeReaderName(reader.name);
+                  readerCtx.onChangeReaderId(reader.id);
+                  history.push(`/reader/${reader.id}/logReading/`);
+                }}
+              >
                 <ListItemIcon>
-                    <PersonAdd fontSize="small" />
+                  <Avatar>{reader.initials}</Avatar>
                 </ListItemIcon>
-                    Add reader
-                </MenuItem>
-                <Divider />
+                {reader.name}
+              </MenuItem>
+            ))}
+          <MenuItem onClick={handleAddReader}>
+            <ListItemIcon>
+              <PersonAdd fontSize="small" />
+            </ListItemIcon>
+            Add reader
+          </MenuItem>
+          <Divider />
 
-                <MenuItem onClick={handleViewPrizes}>
-                <ListItemIcon>
-                    <PrizesIcon fontSize="small" />
-                </ListItemIcon>
-                    Prizes
-                </MenuItem>
-                <MenuItem onClick={handleAddPrize}>
-                <ListItemIcon>
-                    <AddPrizeIcon/>
-                </ListItemIcon>
-                    Add prize
-                </MenuItem>
-                <Divider />
-                <MenuItem onClick={authCtx.onLogout}>
-                    <ListItemIcon>
-                        <LogoutIcon fontSize="small"/>
-                    </ListItemIcon>
-                    Logout
-                </MenuItem>
-            </Menu>
-        </MenuWrapper>
+          <MenuItem onClick={handleViewPrizes}>
+            <ListItemIcon>
+              <PrizesIcon fontSize="small" />
+            </ListItemIcon>
+            Prizes
+          </MenuItem>
+          <MenuItem onClick={handleAddPrize}>
+            <ListItemIcon>
+              <AddPrizeIcon />
+            </ListItemIcon>
+            Add prize
+          </MenuItem>
+          <Divider />
+          <MenuItem onClick={authCtx.onLogout}>
+            <ListItemIcon>
+              <LogoutIcon fontSize="small" />
+            </ListItemIcon>
+            Logout
+          </MenuItem>
+        </Menu>
+      </MenuWrapper>
     </div>
-    );
+  );
 };
 
 export default AccountMenu;
 
-const MenuWrapper = styled.div`
-`;
+const MenuWrapper = styled.div``;
--- client/read-reader/src/pages/Prize/AvailablePrizes.js
+++ client/read-reader/src/pages/Prize/AvailablePrizes.js
@@ -1,88 +1,120 @@
-import { useEffect, useState, useContext} from "react";
+import { useEffect, useState, useContext } from "react";
 
 import AuthContext from "../../store/auth-contex";
 import ReaderContext from "../../store/reader-contex";
 
-import styled from 'styled-components';
+import styled from "styled-components";
 import Prize from "../../components/Prize/Prize";
 
-import { Typography } from '@mui/material'; 
-import { Person, PersonAdd, ArrowRightAlt } from '@mui/icons-material';
-import AddPrizeIcon from "../../components/UI/AddPrizeIcon";
+import { Typography } from "@mui/material";
+import { Person, PersonAdd, ArrowRightAlt } from "@mui/icons-material";
+import AddPrizeIcon from "../../components/Prize/AddPrizeIcon";
 
 const AvailablePrizes = (props) => {
-    const authCtx = useContext(AuthContext);
-    const readerCtx = useContext(ReaderContext);
+  const authCtx = useContext(AuthContext);
+  const readerCtx = useContext(ReaderContext);
 
-    const isMainUser = !readerCtx.currentReaderId;
+  const isMainUser = !readerCtx.currentReaderId;
 
-    const [error, setError] = useState('');
+  const [error, setError] = useState("");
 
-    const [prizes, setPrizes] = useState([]); 
+  const [prizes, setPrizes] = useState([]);
 
-    const [hasPrizes, setHasPrizes] = useState(false);
+  const [hasPrizes, setHasPrizes] = useState(false);
 
-    const hasNoPrizeText = isMainUser ? 'You have not yet created any prizes.' : 'Ask your parent to create prizes you can earn.';
+  const hasNoPrizeText = isMainUser
+    ? "You have not yet created any prizes."
+    : "Ask your parent to create prizes you can earn.";
 
-    useEffect(() => {
-        const url =  isMainUser ? `http://localhost:5000/prizes/` : `http://localhost:5000/reader/${props.readerId}/prizes/available`;
-        const requestOptions = {
-            method: 'GET',
-            headers: {
-                'Accept': 'application/json',
-                Authorization: 'Bearer ' + authCtx.token
-            }
-        };
-        const fetchPrizesData = async() => {
-            const res = await fetch(url, requestOptions);
+  useEffect(() => {
+    const url = isMainUser
+      ? `http://localhost:5000/prizes/`
+      : `http://localhost:5000/reader/${props.readerId}/prizes/available`;
+    const requestOptions = {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: "Bearer " + authCtx.token,
+      },
+    };
+    const fetchPrizesData = async () => {
+      const res = await fetch(url, requestOptions);
 
-            if(!res.ok){
-                throw new Error('Something went wrong!');
-            };
-            const resData = await res.json();
-            const loadedPrizes = resData.prizes;
-            setPrizes(loadedPrizes);
-            setHasPrizes(loadedPrizes.length !== 0);
-        };
+      if (!res.ok) {
+        throw new Error("Something went wrong!");
+      }
+      const resData = await res.json();
+      const loadedPrizes = resData.prizes;
+      setPrizes(loadedPrizes);
+      setHasPrizes(loadedPrizes.length !== 0);
+    };
 
-        fetchPrizesData().catch(err => setError(err.message))
-    }, [authCtx.token]);
+    fetchPrizesData().catch((err) => setError(err.message));
+  }, [authCtx.token]);
 
-    
-
-    return (
+  return (
+    <>
+      <Typography
+        align="center"
+        variant="h2"
+        sx={{ color: "gray", marginTop: "2rem" }}
+      >
+        Prizes
+      </Typography>
+      {props.children && props.children}
+      {hasPrizes ? (
+        <PrizesWrapper>
+          {prizes.map((prize) => (
+            <Prize
+              key={prize._id}
+              prizeName={prize.prize_name}
+              readingRequirement={prize.reading_requirement}
+            />
+          ))}
+        </PrizesWrapper>
+      ) : (
         <>
-        <Typography align="center" variant="h2" sx={{color: "gray", marginTop: '2rem'}}>Prizes</Typography>
-        {props.children && props.children}
-        {hasPrizes ?
-            <PrizesWrapper>
-                {prizes.map(prize => <Prize key={prize._id} prizeName={prize.prize_name} readingRequirement={prize.reading_requirement}/>)}
-            </PrizesWrapper> :
-
-            <>
-                <Typography align="center" variant="h4" component="p" sx={{color: "gray", marginTop: '2rem'}}>{hasNoPrizeText}</Typography>
-                <Typography variant="h6" component="p" sx={{display: 'flex', justifyContent: 'center', color: "gray", marginTop: '1rem'}}><Person fontSize="medium"/> <ArrowRightAlt/>&nbsp;<AddPrizeIcon fontSize="medium"/>Add Prize</Typography>
-            </>
-         }
+          <Typography
+            align="center"
+            variant="h4"
+            component="p"
+            sx={{ color: "gray", marginTop: "2rem" }}
+          >
+            {hasNoPrizeText}
+          </Typography>
+          <Typography
+            variant="h6"
+            component="p"
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              color: "gray",
+              marginTop: "1rem",
+            }}
+          >
+            <Person fontSize="medium" /> <ArrowRightAlt />
+            &nbsp;
+            <AddPrizeIcon fontSize="medium" />
+            Add Prize
+          </Typography>
         </>
-    );
-
+      )}
+    </>
+  );
 };
-
 
 export default AvailablePrizes;
 
-
 const PrizesWrapper = styled.ul`
-    display: grid;
-    grid-gap: 4rem ;
-    padding: 4rem;
-    justify-content: center;
+  display: grid;
+  grid-gap: 4rem;
+  padding: 4rem;
+  justify-content: center;
 
-    @media(min-width: 420px){
-        grid-template-columns: repeat(2, 200px)
-    }
-    @media(min-width: 750px){
-        grid-template-columns: repeat(3, 200px);
-    }
+  @media (min-width: 420px) {
+    grid-template-columns: repeat(2, 200px);
+  }
+  @media (min-width: 750px) {
+    grid-template-columns: repeat(3, 200px);
+  }
 `;
