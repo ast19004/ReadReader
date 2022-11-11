4eacb37c4f0eb8414bfb09c90d877083f59d26df
Anna Astle <ast19004@byui.edu>
2022-11-11 13:36:04 -0800

    Re-route to login after registration

--- client/read-reader/src/components/Reader/ReaderBadge.js
+++ client/read-reader/src/components/Reader/ReaderBadge.js
@@ -1,29 +1,49 @@
+import { useContext } from "react";
 import { Typography } from "@mui/material";
-import styled from 'styled-components';
+import InfoIcon from "@mui/icons-material/Info";
+import styled from "styled-components";
+
+import ReaderContext from "../../store/reader-contex";
 
 const ReaderBadge = (props) => {
+  const readerCtx = useContext(ReaderContext);
 
-    const initials = [...props.readerName].slice(0,2).join("");
-    const capitalizedInitials = initials.charAt(0).toUpperCase() + initials.slice(1)
-    
-    return (
+  const initials = [...props.readerName].slice(0, 2).join("");
+  const capitalizedInitials =
+    initials.charAt(0).toUpperCase() + initials.slice(1);
+
+  return (
     <ReaderBadgeContents>
-        <Typography align="center">{props.minutesRead} Minutes</Typography>
-        <Typography align="center" variant="h2">{capitalizedInitials}</Typography>
-        <Typography align="center">{props.coinsEarned} Coins</Typography>
+      {readerCtx.currentReaderName === "" ? (
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <Typography align="center" variant="h4">
+            {props.readerName}
+          </Typography>
+          <InfoIcon color="action" />
+        </div>
+      ) : (
+        <>
+          {" "}
+          <Typography align="center">{props.minutesRead} Minutes</Typography>
+          <Typography align="center" variant="h2">
+            {capitalizedInitials}
+          </Typography>
+          <Typography align="center">{props.coinsEarned} Coins</Typography>
+        </>
+      )}
     </ReaderBadgeContents>
-    );
+  );
 };
 
 export default ReaderBadge;
 
 const ReaderBadgeContents = styled.li`
-    display: grid;
-    align-items: center;
-    width: 150px;
-    height: 150px;
-    // border: 1px solid #888;
-    border-radius: 50%;
-    padding: 2rem;
-    box-shadow: 3px 3px 10px #666;
+  display: grid;
+  align-items: center;
+  width: 150px;
+  height: 150px;
+  // border: 1px solid #888;
+  border-radius: 50%;
+  padding: 2rem;
+  box-shadow: 3px 3px 10px #666;
 `;
--- client/read-reader/src/pages/AuthUser/AuthRegister.js
+++ client/read-reader/src/pages/AuthUser/AuthRegister.js
@@ -1,142 +1,154 @@
-import { useState } from 'react'; 
-import { Link } from 'react-router-dom';
+import { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 
-import { TextField, Button, Typography} from '@mui/material';
-import styled from 'styled-components';
+import { TextField, Button, Typography } from "@mui/material";
+import styled from "styled-components";
 
 function RegisterUser() {
-    const [error, setError] = useState('');
+  const history = useHistory();
+  const [error, setError] = useState("");
 
-    const [enteredFirstName, setEnteredFirstName] = useState('');
-    const [enteredLastName, setEnteredLastName] = useState('');
-    const [enteredEmail, setEnteredEmail] = useState('');
-    const [enteredPassword, setEnteredPassword] = useState('');
-    const [enteredPasswordConfirm, setEnteredPasswordConfirm] = useState('');
+  const [enteredFirstName, setEnteredFirstName] = useState("");
+  const [enteredLastName, setEnteredLastName] = useState("");
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
+  const [enteredPasswordConfirm, setEnteredPasswordConfirm] = useState("");
 
-    const resetForm = () => {
-        setEnteredFirstName('')
-        setEnteredLastName('')
-        setEnteredEmail('');
-        setEnteredPassword('');
-        setEnteredPasswordConfirm('');
+  const resetForm = () => {
+    setEnteredFirstName("");
+    setEnteredLastName("");
+    setEnteredEmail("");
+    setEnteredPassword("");
+    setEnteredPasswordConfirm("");
+  };
+  const firstNameChangeHandler = (event) => {
+    setEnteredFirstName(event.target.value);
+  };
+
+  const lastNameChangeHandler = (event) => {
+    setEnteredLastName(event.target.value);
+  };
+
+  const emailChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+
+  const passwordChangeHandler = (event) => {
+    setEnteredPassword(event.target.value);
+  };
+
+  const passwordConfirmChangeHandler = (event) => {
+    setEnteredPasswordConfirm(event.target.value);
+  };
+
+  const registerUser = async (event) => {
+    event.preventDefault();
+
+    const url = "http://localhost:5000/user/register";
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        firstName: enteredFirstName,
+        lastName: enteredLastName,
+        email: enteredEmail,
+        password: enteredPassword,
+        confirmPassword: enteredPasswordConfirm,
+      }),
+    };
+    try {
+      const res = await fetch(url, requestOptions);
+      const resData = await res.json();
+      if (resData.errorMessage) {
+        setError(resData.errorMessage);
+      }
+    } catch (err) {
+      setError(err);
     }
-    const firstNameChangeHandler = (event) => {
-        setEnteredFirstName(event.target.value);
-    };
+    history.push("/");
+    resetForm();
+  };
 
-    const lastNameChangeHandler = (event) => {
-        setEnteredLastName(event.target.value);
-    };
-
-    const emailChangeHandler = (event) => {
-        setEnteredEmail(event.target.value);
-    };
-
-    const passwordChangeHandler = (event) => {
-        setEnteredPassword(event.target.value);
-    };
-
-    const passwordConfirmChangeHandler = (event) => {
-        setEnteredPasswordConfirm(event.target.value);
-    };
-
-    const registerUser = async (event) => {
-        event.preventDefault();
-
-        const url = "http://localhost:5000/user/register";
-        const requestOptions = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            body: JSON.stringify({
-                firstName: enteredFirstName,
-                lastName: enteredLastName,
-                email: enteredEmail,
-                password: enteredPassword,
-                confirmPassword: enteredPasswordConfirm
-            })
-        };
-        try{
-        const res = await fetch(url, requestOptions);
-        const resData = await res.json();
-        if(resData.errorMessage){
-            setError(resData.errorMessage);
-        }
-
-    } catch(err){
-        setError(err);
-    }
-
-        resetForm();
-    };
-
-    return (
-        <>
-            <Typography align="center" variant="h4" component={'h2'} sx={{color: "gray", marginTop: '2rem'}}>New Account</Typography>
-            <RegisterForm onSubmit={registerUser}>
-            <TextField
-            onChange={firstNameChangeHandler}
-            value={enteredFirstName}
-            style={{ width: "200px", margin: "5px" }}
-            type="text"
-            label="First Name"
-            variant="outlined"
-            />
-            <br />
-            <TextField
-            onChange={lastNameChangeHandler}
-            value={enteredLastName}
-            style={{ width: "200px", margin: "5px" }}
-            type="text"
-            label="Last Name"
-            variant="outlined"
-            />
-            <br />
-            <TextField
-            onChange={emailChangeHandler}
-            value={enteredEmail}
-            style={{ width: "200px", margin: "5px" }}
-            type="email"
-            label="Email"
-            variant="outlined"
-            />
-            <br />
-            <TextField
-            onChange={passwordChangeHandler}
-            value={enteredPassword}
-            style={{ width: "200px", margin: "5px" }}
-            type="password"
-            label="Password"
-            variant="outlined"
-            />
-            <br />
-            <TextField
-            onChange={passwordConfirmChangeHandler}
-            value={enteredPasswordConfirm}
-            style={{ width: "200px", margin: "5px" }}
-            type="password"
-            label="Confirm Password"
-            variant="outlined"
-            />
-            <br />
-            <Button type="submit" variant="contained" color="primary">
-            Register
-            </Button>
-            {error && <p>{error}</p>}
-            <Link to='/' variant="contained" style={{marginTop: '1rem', textAlign: 'center'}}>Sign In Instead</Link>
-        </RegisterForm>
+  return (
+    <>
+      <Typography
+        align="center"
+        variant="h4"
+        component={"h2"}
+        sx={{ color: "gray", marginTop: "2rem" }}
+      >
+        New Account
+      </Typography>
+      <RegisterForm onSubmit={registerUser}>
+        <TextField
+          onChange={firstNameChangeHandler}
+          value={enteredFirstName}
+          style={{ width: "200px", margin: "5px" }}
+          type="text"
+          label="First Name"
+          variant="outlined"
+        />
+        <br />
+        <TextField
+          onChange={lastNameChangeHandler}
+          value={enteredLastName}
+          style={{ width: "200px", margin: "5px" }}
+          type="text"
+          label="Last Name"
+          variant="outlined"
+        />
+        <br />
+        <TextField
+          onChange={emailChangeHandler}
+          value={enteredEmail}
+          style={{ width: "200px", margin: "5px" }}
+          type="email"
+          label="Email"
+          variant="outlined"
+        />
+        <br />
+        <TextField
+          onChange={passwordChangeHandler}
+          value={enteredPassword}
+          style={{ width: "200px", margin: "5px" }}
+          type="password"
+          label="Password"
+          variant="outlined"
+        />
+        <br />
+        <TextField
+          onChange={passwordConfirmChangeHandler}
+          value={enteredPasswordConfirm}
+          style={{ width: "200px", margin: "5px" }}
+          type="password"
+          label="Confirm Password"
+          variant="outlined"
+        />
+        <br />
+        <Button type="submit" variant="contained" color="primary">
+          Register
+        </Button>
+        {error && <p>{error}</p>}
+        <Link
+          to="/"
+          variant="contained"
+          style={{ marginTop: "1rem", textAlign: "center" }}
+        >
+          Sign In Instead
+        </Link>
+      </RegisterForm>
     </>
-    );
-  }
-  
-  export default RegisterUser;
+  );
+}
 
-  const RegisterForm = styled.form`
+export default RegisterUser;
+
+const RegisterForm = styled.form`
   display: grid;
   grid-template-column: auto;
   justify-content: center;
   margin-top: 2rem;
 `;
-
